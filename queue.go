6d653f58a2d9e0a911a9db2a27adc8f824e80d18
JorgeLNJunior <jorgelnjunior@gmail.com>
2024-08-22 18:48:52 -0300

    feat: add a method to enqueue jobs in batch

--- queue.go
+++ queue.go
@@ -3,25 +3,48 @@ package queuelite
 import (
 	"context"
 	"database/sql"
+	"runtime"
 	"time"
 )
 
 type SqlQueue struct {
-	db *sql.DB
+	db      *sql.DB
+	writeDB *sql.DB
+	readDB  *sql.DB
 }
 
-func NewSQLiteQueue(db *sql.DB) (*SqlQueue, error) {
+func NewSQLiteQueue(db string) (*SqlQueue, error) {
 	ctx := context.Background()
 	timeoutCtx, cancel := context.WithTimeout(ctx, time.Second*15)
 	defer cancel()
 
-	err := createTables(timeoutCtx, db)
+	writeDB, err := sql.Open("sqlite", db+"")
+	if err != nil {
+		return nil, err
+	}
+	writeDB.SetMaxOpenConns(1)
+
+	readDB, err := sql.Open("sqlite", db+"")
+	if err != nil {
+		return nil, err
+	}
+	readDB.SetMaxOpenConns(max(4, runtime.NumCPU()))
+
+	if err := writeDB.Ping(); err != nil {
+		return nil, err
+	}
+	if err := readDB.Ping(); err != nil {
+		return nil, err
+	}
+
+	err = createTables(timeoutCtx, writeDB)
 	if err != nil {
 		return nil, err
 	}
 
 	return &SqlQueue{
-		db: db,
+		writeDB: writeDB,
+		readDB:  readDB,
 	}, nil
 }
 
@@ -35,6 +58,33 @@ func (q *SqlQueue) Enqueue(ctx context.Context, job Job) error {
 	)
 	if err != nil {
 		return nil
+	}
+
+	return nil
+}
+
+func (q *SqlQueue) BatchEnqueue(ctx context.Context, jobs []Job) error {
+	tx, err := q.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	for _, job := range jobs {
+		_, err := tx.ExecContext(
+			ctx,
+			"INSERT INTO queuelite_job (id, status, data) VALUES (?, ?, ?)",
+			job.ID,
+			job.Status,
+			job.Data,
+		)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
 	}
 
 	return nil
